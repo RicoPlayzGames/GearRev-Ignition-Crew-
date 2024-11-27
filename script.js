5937399510806dfffbde40addd5d93f4c861f679
RicoPlayzGames <ek.borgdorff@gmail.com>
2024-11-27 11:10:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-btn').addEventListener('click', function() {
+    // Replace with your Outlook email address
+    window.location.href = 'mailto:rico.borgdorff@outlook.com?subject=Contact from GearRev Ignition Crew+&body=Hello, I have a question about the app...';
+});
